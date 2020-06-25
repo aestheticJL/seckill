a40d916e67c4a5bdbaa31119a015c947c68b086d
aestheticJL <m258789@126.com>
2020-06-25 16:42:08 +0800

    初始化

---
+++ seckill-server/seckill-web/src/main/java/com/mmt/seckill/SeckillApplication.java
@@ -0,0 +1,18 @@
+package com.mmt.seckill;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(scanBasePackages = {"com.mmt"})
+@MapperScan("com.mmt.seckill.mapper")
+@EnableScheduling
+@EnableCaching
+public class SeckillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+}
