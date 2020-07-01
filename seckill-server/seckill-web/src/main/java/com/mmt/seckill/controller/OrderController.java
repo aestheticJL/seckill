07ff75bfa8faf4084e700f4ed6f623bf4e8d6f0b
aestheticJL <m258789@126.com>
2020-07-01 22:51:20 +0800

    使用拥塞窗口泄洪

--- seckill-server/seckill-web/src/main/java/com/mmt/seckill/controller/OrderController.java
+++ seckill-server/seckill-web/src/main/java/com/mmt/seckill/controller/OrderController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.util.concurrent.*;
+
 @RestController
 public class OrderController {
     @Autowired
@@ -23,6 +26,13 @@ public class OrderController {
     private PromoService promoService;
     @Autowired
     private UserService userService;
+
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    @PostConstruct
+    public void initThreadPoolExecutor() {
+        threadPoolExecutor = new ThreadPoolExecutor(20, 20, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+    }
 
     @PostMapping("/itemToken")
     public String createPromoToken(@RequestParam(name = "userId") Integer userId,
@@ -39,11 +49,11 @@ public class OrderController {
     }
 
     @PostMapping("/order")
-    public RespBean createOrder(@RequestParam(name = "userId") Integer userId,
-                                @RequestParam(name = "itemId") Integer itemId,
-                                @RequestParam(name = "amount") Integer amount,
-                                @RequestParam(name = "promoId", required = false) Integer promoId,
-                                @RequestParam(name = "promoToken", required = false) String promoToken) {
+    public void createOrder(@RequestParam(name = "userId") Integer userId,
+                            @RequestParam(name = "itemId") Integer itemId,
+                            @RequestParam(name = "amount") Integer amount,
+                            @RequestParam(name = "promoId", required = false) Integer promoId,
+                            @RequestParam(name = "promoToken", required = false) String promoToken) {
         String redisToken = (String) redisTemplate.opsForValue().get("promo" + promoId + "_item" + itemId + "_user" + userId + "_token");
         if (redisToken == null) {
             throw new RuntimeException("令牌检测失败");
@@ -51,13 +61,24 @@ public class OrderController {
         if (!redisToken.equals(promoToken)) {
             throw new RuntimeException("令牌检测失败");
         }
-        //创建订单流水
-        String stockLogId = stockLogService.createStockLog(itemId, amount);
-        //发送一个事务型消息给消息队列
-        if (mqProducer.transactionAsyncReduceStock(userId, itemId, amount, promoId, stockLogId)) {
-            return RespBean.ok("下单成功");
-        } else {
-            return RespBean.error("下单失败");
+        Future<Object> future = threadPoolExecutor.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                //创建订单流水
+                String stockLogId = stockLogService.createStockLog(itemId, amount);
+                //发送一个事务型消息给消息队列
+                if (!mqProducer.transactionAsyncReduceStock(userId, itemId, amount, promoId, stockLogId)) {
+                    return RespBean.error("下单失败");
+                }
+                return RespBean.ok("下单成功");
+            }
+        });
+        try {
+            future.get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("未知错误");
+        } catch (ExecutionException e) {
+            throw new RuntimeException("未知错误");
         }
     }
 }
